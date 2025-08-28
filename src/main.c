f0a8d8c1d9f383925fabc9b3a629b325d71e0d5e
Felipe Lisandro <felipelisandro97@gmail.com>
2025-08-28 02:25:40 -0300

    Main

--- src/main.c
+++ src/main.c
@@ -1,36 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include "input.h"
-#include "execute.h"
-#include <string.h>
+#include "terminal.h"
 
 int main(void){
-    char **arrayDir = getSysPaths();
-    if(!arrayDir){
-        fprintf(stderr, "Could not get the System Paths.\n");
-        return 1;
-    }
-    printf("Simple Shell initialized\n");
-    while(1){
-        printf("@simpleshell ");
-        char **arrayCom = inputCom();
-        if(!arrayCom) continue; // check if fail
-        if(!arrayCom[0]){
-            free(arrayCom);
-            continue; // check if nothing was inputed
-        }
-        if(strcmp(arrayCom[0], "exit") == 0){ // check if program should exit
-            for(int i = 0; arrayCom[i]; i++) free(arrayCom[i]);
-            free(arrayCom);
-            break;
-        }
-        // finally, executes the command
-        receiveCommand(arrayDir, arrayCom);
-    }
-    // as the program exits, the arrayDir should be fred
-    for(int i = 0; arrayDir[i]; i++) free(arrayDir[i]);
-    free(arrayDir);
-    return 0;
+    int ret = run();
+    return ret;
 }
